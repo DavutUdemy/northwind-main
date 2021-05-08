06b98c937b8c82fec13a138a4e018046482524a1
macbookair <thesame@email.com>
2021-05-09 01:31:10 +0400

    commit

---
+++ northwind-main/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -0,0 +1,122 @@
+package kodlamaio.northwind.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "products")
+
+public class Product {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "product_id")
+	private int id;
+	
+	@Column(name = "category_id")
+	private int categoryId;
+	
+	@Column(name = "product_name")
+	private String productName;
+	
+	@Column(name = "unit_price")
+	private double unitPrice;
+	
+	@Column(name = "units_in_stock")
+	private short unitsInStock;
+	
+	@Column(name = "quantity_per_unit")
+	private String quantityPerUnit;
+	public Product() {}
+	
+	public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock,
+			String quantityPerUnit) {
+		super();
+		this.id = id;
+		this.categoryId = categoryId;
+		this.productName = productName;
+		this.unitPrice = unitPrice;
+		this.unitsInStock = unitsInStock;
+		this.quantityPerUnit = quantityPerUnit;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the categoryId
+	 */
+	public int getCategoryId() {
+		return categoryId;
+	}
+	/**
+	 * @param categoryId the categoryId to set
+	 */
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	/**
+	 * @return the productName
+	 */
+	public String getProductName() {
+		return productName;
+	}
+	/**
+	 * @param productName the productName to set
+	 */
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	/**
+	 * @return the unitPrice
+	 */
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+	/**
+	 * @param unitPrice the unitPrice to set
+	 */
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	/**
+	 * @return the unitsInStock
+	 */
+	public short getUnitsInStock() {
+		return unitsInStock;
+	}
+	/**
+	 * @param unitsInStock the unitsInStock to set
+	 */
+	public void setUnitsInStock(short unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+	/**
+	 * @return the quantityPerUnit
+	 */
+	public String getQuantityPerUnit() {
+		return quantityPerUnit;
+	}
+	/**
+	 * @param quantityPerUnit the quantityPerUnit to set
+	 */
+	public void setQuantityPerUnit(String quantityPerUnit) {
+		this.quantityPerUnit = quantityPerUnit;
+	}
+	
+}
